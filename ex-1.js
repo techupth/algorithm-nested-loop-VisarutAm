31df78f1634a603d58897724dbe5195d416b1784
Am Visarut <visarut.thk@gmail.com>
2024-07-03 16:17:09 +0700

    Nested Loop

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,14 @@
 // เริ่มเขียนโค้ดตรงนี้ 
-function printPattern() {
-   
+function printPattern(n) {
+    let finalResult ="";
+    for (let i=1;i<=n;i++) {
+   let rowResult = ""; 
+   for(let j = 1; j <= i; j++) {
+    rowResult += j + "\t";
+   }
+   finalResult += rowResult + "\n";
+  }
+  return finalResult;
 }
+console.log(printPattern(5))
+
--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,13 @@
 // เริ่มเขียนโค้ดตรงนี้ 
-function printStar() {
-   
+function printStar(n) {
+    let finalResult ="";
+    for (let i=1;i<=n;i++) {
+   let rowResult = ""; 
+   for(let j = 1; j <= i; j++) {
+    rowResult += "*" + "\t";
+   }
+   finalResult += rowResult + "\n";
+  }
+  return finalResult;
 }
+console.log(printStar(5))
